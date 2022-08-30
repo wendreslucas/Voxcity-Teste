65f9bd574c69fccecc68d749041e764531d32773
Wendres Lucas <wendreslucas@gmail.com>
2022-08-30 16:06:42 -0300

    implements hook useForm

---
+++ src/hooks/useForm.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+function useForm(valoresIniciais) {
+  const [values, setValues] = useState(valoresIniciais);
+  function setValue(chave, valor) {
+    setValues({
+      ...values,
+      [chave]: valor,
+    });
+  }
+
+  function handleChange(e) {
+    setValue(e.target.getAttribute("name"), e.target.value);
+  }
+
+  function clearForm() {
+    setValues(valoresIniciais);
+  }
+
+  return {
+    values,
+    handleChange,
+    clearForm,
+  };
+}
